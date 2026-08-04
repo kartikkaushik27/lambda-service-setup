2eccba3b44294323da523a8437f0eb6ae69210be
Kartik Kaushik <kartik.kaushik@harness.io>
2026-08-04 14:43:09 -0500

    Disable delegate execution for GitHub connector connectivity checks

--- harness_connectors.tf
+++ harness_connectors.tf
@@ -13,12 +13,13 @@ resource "harness_platform_connector_aws" "this" {
 }
 
 resource "harness_platform_connector_github" "this" {
-  identifier      = "github_connector"
-  name            = "GitHub Connector"
-  org_id          = var.harness_org_id
-  project_id      = harness_platform_project.this.identifier
-  url             = "https://github.com/${var.github_owner}/${var.github_repo_name}"
-  connection_type = "Repo"
+  identifier          = "github_connector"
+  name                = "GitHub Connector"
+  org_id              = var.harness_org_id
+  project_id          = harness_platform_project.this.identifier
+  url                 = "https://github.com/${var.github_owner}/${var.github_repo_name}"
+  connection_type     = "Repo"
+  execute_on_delegate = false
 
   credentials {
     http {
